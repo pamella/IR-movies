195b5a189e91ebdf876a55bb617c52af9b2e0164
pamella <pammbsilva@gmail.com>
2019-11-21 02:08:33 -0300

    Add inverted index search

--- inverted_index/query.py
+++ inverted_index/query.py
@@ -30,23 +30,31 @@ with open('inverted_index/data/html_docs_tokens_true.json') as f:
     f.close()
 
 
-def get_websites_list(html_docs, ids_list):
+def get_websites_list(html_docs_type, ids_list):
     links = []
+    if html_docs_type == 'true':
+        html_docs = html_docs_tokens_true
+    else:
+        html_docs = html_docs_tokens_all
     for id in ids_list:
         if html_docs.get(id):
             links.append(html_docs.get(id)[1])
     return links
 
-import ipdb ; ipdb.set_trace()
+
+def get_search_result(html_docs_type, search_field, search_value):
+    result_docs = search(search_field, search_value)
+    result_links = get_websites_list(html_docs_type, result_docs)
+    return result_links
 
 # result_docs = search('title', 'breakfast club')
-search_field = input('Digite o field que deseja buscar (title, year, genre)\n')
-search_value = input('Digite o valor que deseja buscar (ex.: imitation game)\n')
+# search_field = input('Digite o field que deseja buscar (title, year, genre)\n')
+# search_value = input('Digite o valor que deseja buscar (ex.: imitation game)\n')
 
-result_docs = search(search_field, search_value)
-result_links_all = get_websites_list(html_docs_tokens_all, result_docs)
-result_links_true = get_websites_list(html_docs_tokens_true, result_docs)
+# result_docs = search(search_field, search_value)
+# result_links_all = get_websites_list("all", result_docs)
+# result_links_true = get_websites_list("true", result_docs)
 
-print(f'Docs id:\n {result_docs}\n')
-print(f'All:\n {result_links_all}\n')
-print(f'True:\n {result_links_true}\n')
+# print(f'Docs id:\n {result_docs}\n')
+# print(f'All:\n {result_links_all}\n')
+# print(f'True:\n {result_links_true}\n')
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from movies.models import Genre, Movie
+from inverted_index.query import get_search_result as inverted_index_search
 
 
 class MovieListView(generic.ListView):
@@ -9,16 +10,26 @@ class MovieListView(generic.ListView):
     template_name = 'movies/list.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        query_type = self.request.GET.get("query_type")
         select = self.request.GET.get("select")
         search = self.request.GET.get("search")
         if search and select:
-            if select == 'genres':
-                genre = Genre.objects.get(name=search)
-                object_list = self.get_queryset().filter(genres__in=[genre])
-            else:
-                object_list = self.get_queryset().filter(**{f"{select}__icontains": search})
+            if query_type == 'django':
+                if select == 'genres':
+                    genre = Genre.objects.get(name=search)
+                    object_list = self.get_queryset().filter(genres__in=[genre])
+                else:
+                    object_list = self.get_queryset().filter(**{f"{select}__icontains": search})
+
             context = super().get_context_data(**kwargs, object_list=object_list)
             context["search_term"] = search
+
+            if query_type == 'inverted-index':
+                if select == 'genres':
+                    select = 'genre'
+                context["inverted_index_search"] = True
+                context["inverted_index_search_all"] = inverted_index_search("all", select, search)
+                context["inverted_index_search_true"] = inverted_index_search("true", select, search)
             return context
         return super().get_context_data(**kwargs)
 
