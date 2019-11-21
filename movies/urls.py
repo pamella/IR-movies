85589b97ef4c8b14017d6e2dfee5797a71a4dc06
pamella <pammbsilva@gmail.com>
2019-11-21 01:17:09 -0300

    Add movie list view and its initial template

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from movies.views import MovieListView
 
 
 app_name = 'movies'
 
 urlpatterns = [
+    path('', MovieListView.as_view(), name='movie-list'),
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
+from movies.models import Genre, Movie
 
-# Create your views here.
+
+class MovieListView(generic.ListView):
+    model = Movie
+    context_object_name = 'movies'
+    template_name = 'movies/list.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        select = self.request.GET.get("select")
+        search = self.request.GET.get("search")
+        if search and select:
+            object_list = self.get_queryset().filter(**{f"{select}__iexact": search})
+            context = super().get_context_data(**kwargs, object_list=object_list)
+            context["search_term"] = search
+            return context
+        return super().get_context_data(**kwargs)
+
